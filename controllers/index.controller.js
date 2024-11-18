e4051505f0ea1e4838c0f10e490eaa2a516ea2f0
amir <hasanabadi.am1380@gmail.com>
2024-11-18 08:57:44 +0330

    Feat: add category controller added

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,14 +1,29 @@
 const {Category} = require("../models/category.model");
 
-exports.addCategory = (req, rep) => {
+exports.addCategory = async (req, rep) => {
     try {
+        if (!req.body) {
+            return rep.status(400).send({message: "request body can not be empty"});
+        }
         const {title, description} = req.body;
 
         if (!title) {
             return rep.status(400).send({message: "Title is required"});
         }
 
-        return rep.status(200).send({message: "category added successfully"})
+        const targeted = await Category.findOne({title: title});
+        if (targeted) {
+            return rep.status(409).send({message: "category already defined"});
+        }
+
+        let newCategory = new Category({
+            title: title,
+            description: description
+        })
+
+        newCategory = await newCategory.save();
+
+        return rep.status(200).send({message: "category added successfully", data: newCategory});
     } catch (error) {
         return rep.status(500).send({ message: `internal error - ${error.message}` });
     }
