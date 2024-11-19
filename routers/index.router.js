875373d27e287b9610ea4fbc8312f2a80be3aae1
amir <hasanabadi.am1380@gmail.com>
2024-11-19 19:11:38 +0330

    Feat: GET method - swagger finished

--- routers/index.router.js
+++ routers/index.router.js
@@ -1,20 +1,53 @@
 const indexController = require("../controllers/index.controller");
 
 module.exports = function (fastify, otps, next) {
-    fastify.post("/add-category", indexController.addCategory);
-    fastify.post("/add-product", indexController.addProduct);
-    fastify.get("/search-product", {schema: {
-      description: "get some data",
-      tags: ["search"],
-      params: {
-        type: "object",
-        properties: {
-          query: {
-            type: "string",
+  fastify.post("/add-category", indexController.addCategory);
+  fastify.post("/add-product", indexController.addProduct);
+  fastify.get(
+    "/search-product",
+    {
+      schema: {
+        querystring: {
+          type: "object",
+          required: ["category", "title"],
+
+          properties: {
+            category: {
+              type: "string",
             },
+            title: {
+              type: "string",
+            },
+          },
+        },
+        response: {
+          200: {
+            description: "Successful response",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+              data: { type: "array" },
+            },
+          },
+          400: {
+            description: "Bad request",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+          500: {
+            description: "Internal server error",
+            type: "object",
+            properties: {
+              message: { type: "string" }
             }
-      }
-    }}, indexController.searchProduct);
-    fastify.post("/change-category-name", indexController.changeCategoryName);
-    next();
-  };
+          }
+        },
+      },
+    },
+    indexController.searchProduct
+  );
+  fastify.post("/change-category-name", indexController.changeCategoryName);
+  next();
+};
