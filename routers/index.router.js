2000381b7f7fb5ae447ff246abd97a896728e5c5
amir <hasanabadi.am1380@gmail.com>
2024-11-19 15:18:01 +0330

    Feat: swagger info for get method added

--- routers/index.router.js
+++ routers/index.router.js
@@ -3,7 +3,18 @@ const indexController = require("../controllers/index.controller");
 module.exports = function (fastify, otps, next) {
     fastify.post("/add-category", indexController.addCategory);
     fastify.post("/add-product", indexController.addProduct);
-    fastify.get("/search-product", indexController.searchProduct);
+    fastify.get("/search-product", {schema: {
+      description: "get some data",
+      tags: ["search"],
+      params: {
+        type: "object",
+        properties: {
+          query: {
+            type: "string",
+            },
+            }
+      }
+    }}, indexController.searchProduct);
     fastify.post("/change-category-name", indexController.changeCategoryName);
     next();
   };
