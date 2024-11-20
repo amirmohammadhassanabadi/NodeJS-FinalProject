3e1cae0a16df5972d1caf8f1732e27a8322b2ade
amir <hasanabadi.am1380@gmail.com>
2024-11-20 12:33:34 +0330

    Feat: add-category swagger added

--- routers/index.router.js
+++ routers/index.router.js
@@ -1,7 +1,71 @@
 const indexController = require("../controllers/index.controller");
 
 module.exports = function (fastify, otps, next) {
-  fastify.post("/add-category", indexController.addCategory);
+  fastify.post(
+    "/add-category",
+    {
+      schema: {
+        description: "Add a new category",
+        body: {
+          type: "object",
+          required: ["title"],
+          properties: {
+            title: {type: "string"},
+            description: {type: "string"},
+            subCategory: {
+              type: "object",
+            }
+          },
+        },
+        // parameters: {
+        //   in: "body",
+        //   name: "add-category",
+        //   description: "Add a new category",
+        //   schema: {
+        //     type: "object",
+        //     required: ["title"],
+        //     properties: {
+        //       title: {type: "string"},
+        //       description: {type: "string"},
+        //       subCategory: {type: "array"}
+        //     }
+        //   }
+        // },
+        response: {
+          200: {
+            description: "Successful response",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+              data: { type: "object" },
+            },
+          },
+          400: {
+            description: "Bad request",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+          409: {
+            description: "category already defined",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+          500: {
+            description: "Internal server error",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    indexController.addCategory
+  );
   fastify.post("/add-product", indexController.addProduct);
   fastify.get(
     "/search-product",
@@ -40,56 +104,60 @@ module.exports = function (fastify, otps, next) {
             description: "Internal server error",
             type: "object",
             properties: {
-              message: { type: "string" }
-            }
-          }
+              message: { type: "string" },
+            },
+          },
         },
       },
     },
     indexController.searchProduct
   );
-  fastify.put("/change-category-name", {
-    schema: {
-      body: {
-        type: "object",
-        required: ["current", "newCategory"],
-        properties: {
-          current: { type: "string" },
-          newCategory: { type: "string" }
+  fastify.put(
+    "/change-category-name",
+    {
+      schema: {
+        body: {
+          type: "object",
+          required: ["current", "newCategory"],
+          properties: {
+            current: { type: "string" },
+            newCategory: { type: "string" },
+          },
+        },
+        response: {
+          200: {
+            description: "Successful response",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+              data: { type: "object" },
+            },
+          },
+          400: {
+            description: "Bad request",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+          404: {
+            description: "Category not found",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+          500: {
+            description: "Internal server error",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
         },
       },
-      response: {
-        200: {
-          description: "Successful response",
-          type: "object",
-          properties: {
-            message: { type: "string" },
-            data: { type: "object" },
-          },
-        },
-        400: {
-          description: "Bad request",
-          type: "object",
-          properties: {
-            message: { type: "string" },
-          },
-        },
-        404: {
-          description: "Category not found",
-          type: "object",
-          properties: {
-            message: { type: "string" }
-          }
-        },
-        500: {
-          description: "Internal server error",
-          type: "object",
-          properties: {
-            message: { type: "string" }
-          }
-        }
-      }
-    }
-  }, indexController.changeCategoryName);
+    },
+    indexController.changeCategoryName
+  );
   next();
 };
